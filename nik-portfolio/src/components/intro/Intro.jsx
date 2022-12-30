020bdb903bdbb93f15a722a8dabdfd026917d9ca
nikhilkalhan92 <nkalhan@hotmail.co.uk>
2022-12-30 16:46:01 +0000

    added classnames for front page and styled it with css

--- nik-portfolio/src/components/intro/Intro.jsx
+++ nik-portfolio/src/components/intro/Intro.jsx
@@ -1,7 +1,27 @@
+
+
 const Intro = () => {
     return (
-        <div>
-            this is intro 
+        <div className="i">
+            <div className="i-left">
+                <div className="i-left-wrapper">
+                     <h2 className="i-intro">Hello, my name is</h2>
+                     <h1 className="i-name">Nikhil Kalhan </h1>
+                     <div className="i-title">
+                       <div className="i-title-wrapper">
+                        <div className="i-title-item">Web Developer</div>
+                        <div className="i-title-item">UI/UX Designer</div>
+                        <div className="i-title-item">Front/Backend</div>
+                        <div className="i-title-item">Full Stack Developer</div>
+                        <div className="i-title-item">Sports Fan</div>
+                        <div className="i-title-item">Hate WOKE SHIT </div>
+
+                       </div>
+                     </div>
+                </div>
+
+                </div>
+            <div className="i-right">right</div>
         </div>
     )
 }
