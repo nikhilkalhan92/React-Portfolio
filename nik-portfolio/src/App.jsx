fa55c0c24c56851783a692665a869f27a834fdaf
nikhilkalhan92 <nkalhan@hotmail.co.uk>
2022-12-31 01:33:14 +0000

    added about section styling it with pictures and css

--- nik-portfolio/src/App.jsx
+++ nik-portfolio/src/App.jsx
@@ -1,8 +1,10 @@
+import About from "./components/about/About";
 import Intro from "./components/intro/Intro";
 
 const App = () => {
   return (
-    <div><Intro/></div>
+    <><div><Intro/></div>
+    <div><About/></div></>
   )
 }
 export default App;
