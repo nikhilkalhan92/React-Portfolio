407fcd9b366c2780f4bd89a4d528d94baba66b3c
nikhilkalhan92 <84812103+nikhilkalhan92@users.noreply.github.com>
2023-01-10 19:25:02 +0000

    last amendments

--- nik-portfolio/src/components/about/About.jsx
+++ nik-portfolio/src/components/about/About.jsx
@@ -10,9 +10,9 @@ const About = () => {
                                     
                   <a href='https://www.credential.net/99be0a18-78b1-4251-b9ae-fded475a7159#gs.84w7f4'> - Diploma in Full Stack Software Development (E-commerce Applications)</a>
 
-                   <p> <a href='https://github.com/nikhilkalhan92'> - Click here for Github Portfolio</a> </p>
+                   <p> <a href='https://github.com/nikhilkalhan92'> - Github</a> </p>
 
-                   <p> <a href='https://www.dropbox.com/s/rk5r5pxnhxnar2c/NikhilKalhanCV22%20%284%29.pdf?dl=0'>- Nikhil CV </a></p>
+                   <p> <a href='https://www.dropbox.com/s/rk5r5pxnhxnar2c/NikhilKalhanCV22%20%284%29.pdf?dl=0'> - Nikhil CV </a></p>
 
                    
                 
